572e93218070a151ebd365f5ecb83fb7326a43ec
Seven Yu <dofyyu@gmail.com>
2023-11-15 15:28:28 +0800

    update demo file

--- zh-CN/chapter04-demo.js
+++ zh-CN/chapter04-demo.js
@@ -1,8 +1,8 @@
-function(win, doc) { // 光标定位到前面的｛ 然后按 =%
+function myFunction() { // 光标定位到前面的｛ 然后按 =%
 var n = ((1 + 2) * (3 + 4)) / 7;
 var a = [1, 2, 3, 4, 5, 6, 7];
-var f = function(b) {
-if(b) {
+var f = function (b) {
+if (b) {
 return false;
 } else {
 return true;
